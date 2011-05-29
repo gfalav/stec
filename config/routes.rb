a5e6051efb0335d71dda374a9a86dbf879a83bcd
Gustavo <gfalav@hotmail.com>
2011-05-29 12:44:33 -0300

    arreglado routes

--- config/routes.rb
+++ config/routes.rb
@@ -60,7 +60,7 @@ Stec::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+   root :to => "tasks#avance"
 
   # See how all your routes lay out with "rake routes"
 
